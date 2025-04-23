40004e437712762fb4178e10afcbe435404726d5
Dimitris Kougioumtzis <dimkoug@gmail.com>
2025-04-24 02:11:01 +0300

    wip

--- worldpostcodes/geonames/views.py
+++ worldpostcodes/geonames/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.cache import cache
 from django.views.generic import TemplateView
 # Create your views here.
 from rest_framework import viewsets
@@ -21,10 +22,19 @@ class RawGeoNameGeoViewSet(viewsets.ReadOnlyModelViewSet):
     pagination_class = RawGeoNamePagination
 
     def get_queryset(self):
-        qs = RawGeoName.objects.exclude(point__isnull=True)
-        country = self.request.query_params.get('country_code')
-        if country:
-            qs = qs.filter(country_code=country)
+        query_params = self.request.query_params
+        # Generate a sorted key string from query params
+        sorted_items = sorted(query_params.items())
+        key = 'geo_queryset_' + '_'.join(f'{k}-{v}' for k, v in sorted_items)
+        print(f"Cache key: {key}")
+        qs = cache.get(key)
+        if not qs:
+            print(f"{key} not exists")
+            # qs = RawGeoName.objects.exclude(point__isnull=True)
+            country = self.request.query_params.get('country_code')
+            if country:
+                qs = RawGeoName.objects.filter(country_code=country)
+            cache.set(key, qs, 86400)
         return qs
     
 
